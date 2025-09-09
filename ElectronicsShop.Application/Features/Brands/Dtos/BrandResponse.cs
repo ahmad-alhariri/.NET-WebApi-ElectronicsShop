68ee422c0f20d9cec889277b29007d1c050c2894
ahmad-alhariri <programer069@gmail.com>
2025-09-09 18:58:00 +0300

    refactor(brand): modify response to include product count

--- ElectronicsShop.Application/Features/Brands/Dtos/BrandResponse.cs
+++ ElectronicsShop.Application/Features/Brands/Dtos/BrandResponse.cs
@@ -1,3 +1,3 @@
 namespace ElectronicsShop.Application.Features.Brands.Dtos;
 
-public sealed record BrandResponse(int Id, string Name, string LogoUrl, DateTime CreatedDate, DateTime UpdatedDate);
+public sealed record BrandResponse(int Id, string Name, string LogoUrl, int ProductCount, DateTime CreatedDate, DateTime UpdatedDate);
